260f02aa8386f1e1b81e588a05ac01af6e7b8f9f
Aiden Bai <aiden.bai05@gmail.com>
2026-03-12 21:18:13 -0700

    bump

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,4 +3,5 @@ export { DEFAULT_TIMEOUT_MS, MS_PER_SECOND } from "./constants";
 export { execCommand } from "./exec-command";
 export { formatError } from "./format-error";
 export { getEpochSeconds } from "./get-epoch-seconds";
+export { isRecord } from "./is-record";
 export { sleep } from "./sleep";
